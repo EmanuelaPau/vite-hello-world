3c79f1e1e735c2472c8465172f2bf4ab0b4a5f04
EmanuelaPau <uapame@gmail.com>
2023-06-14 16:48:11 +0200

    Add reset

--- vite-project/src/components/Jumbo.vue
+++ vite-project/src/components/Jumbo.vue
@@ -10,4 +10,11 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.my_Jumbo {
+    width: 100%;
+    height: 200px;
+    text-align: center;
+    background-color: aqua;
+}
+</style>
