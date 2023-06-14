08a1075b5960d5c37fefef277fa28bc93f420218
EmanuelaPau <uapame@gmail.com>
2023-06-14 16:33:26 +0200

    Clean vue

---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="my_title">This is my Title</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
